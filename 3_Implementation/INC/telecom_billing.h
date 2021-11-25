bb1f68c1d237b8d455f797c0688bdf6d27ab72b0
santhosh1120 <94516294+santhosh1120@users.noreply.github.com>
2021-11-25 11:36:35 +0530

    Update telecom_billing.h

--- 3_Implementation/INC/telecom_billing.h
+++ 3_Implementation/INC/telecom_billing.h
@@ -1,1 +1,25 @@
+#ifndef __TELECOM_BILLING_H__
+#define __TELECOM_BILLING_H__
+
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<windows.h>
+#include<stdlib.h>
+
+void addrecords();
+void listrecords();
+void modifyrecords();
+void deleterecords();
+void searchrecords();
+void payment();
+
+typedef struct subscriber
+{
+char phonenumber[20];
+char name[50];
+float amount;
+}s;
+
+#endif
 
